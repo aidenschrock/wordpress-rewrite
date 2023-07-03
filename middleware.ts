d8862a65b7b064251abe1ab1c49a9d9a53dcebd9
Aiden Schrock <68505349+aidenschrock@users.noreply.github.com>
2023-07-03 12:55:19 -0500

    update vercel.json

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from "next/server"
 
 export default async function middleware(req: NextRequest) {
-    const url = req.nextUrl
-    if (url.href.includes("/blog")) {
-        const postPath = url.href.split("/blog")
-        console.log(postPath)
-        return NextResponse.rewrite(`https://dbredvick4d3f490f3a.wordpress.com${postPath.length >= 1 ? postPath[1] : ""}/`)
-    }
+    // const url = req.nextUrl
+    // if (url.href.includes("/blog")) {
+    //     const postPath = url.href.split("/blog")
+    //     console.log(postPath)
+    //     return NextResponse.rewrite(`https://dbredvick4d3f490f3a.wordpress.com${postPath.length >= 1 ? postPath[1] : ""}/`)
+    // }
 }
